8a960884d9665053d91e32c2bc22fd0e61497374
Srija Ghosh <srijaghosh2015@gmail.com>
2021-08-04 16:17:42 -0400

    screens

--- searchableTable/ViewController.swift
+++ searchableTable/ViewController.swift
@@ -7,8 +7,15 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tblView: UITableView!
+    
+    var statesNameArr = ["Maine","Texas","Rhode Island","Hawaii","Massachusetts"]
+    var searchState = [String]()
+    var searching = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +24,33 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if searching {
+            cell?.textLabel?.text = searchState[indexPath.row]
+        }
+        else {
+            cell?.textLabel?.text = statesNameArr[indexPath.row]
+        }
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            return searchState.count
+        }
+        else {
+            return statesNameArr.count
+        }
+    }
+}
+
+extension ViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchState = statesNameArr.filter({$0.prefix(searchText.count) == searchText})
+        searching = true
+        tblView.reloadData()
+    }
+}
